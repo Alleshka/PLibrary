fca214150e0af352a702865c36fc889afe8be175
grebovod_aleksei@mail.ru <grebovod_aleksei@mail.ru>
2017-10-06 16:45:14 +0300

    Vector and Point

---
+++ AGLibrary/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AGLibrary.Vector
+{
+    public class Point:Vector<double>
+    {
+        public Point(double a, double b) : base(new double[2] { a, b })
+        {
+
+        }
+
+        public double X
+        {
+            get => this[0];
+            set => this[0] = value;
+        }
+
+        public double Y
+        {
+            get => this[1];
+            set => this[1] = value;
+        }
+    }
+}
---
+++ AGLibrary/Vector.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AGLibrary.Vector
+{
+    // Класс вектор
+    public class Vector<T> : IEnumerable
+    {
+        // Пригодится дял печати вектора 
+        public delegate void print(Vector<T> vector);
+
+        private T[] _numbers; // Содержимое вектора 
+        // Размер вектора
+        public int Lengh
+        {
+            get => _numbers.Length;
+        }
+
+        /// <summary>
+        /// Создаёт вектор с указанным размером и инициализирует по умолчанию
+        /// </summary>
+        /// <param name="n">Размер вектора</param>
+        public Vector(int n)
+        {
+            _numbers = new T[n];
+            NullInit();
+        }
+        /// <summary>
+        /// Инициализация вектора входными последовательностями
+        /// </summary>
+        public Vector(T[] vector) => _numbers = (T[])vector.Clone();
+        public Vector(List<T> vector) => _numbers = (T[])vector.ToArray().Clone();
+
+        /// Устанавливаем значение по умолчанию
+        public void NullInit()
+        {
+            for (int i = 0; i < _numbers.Length; i++) _numbers[i] = default(T);
+        }
+        // Настраиваем индексатор
+        public T this[int index]
+        {
+            get => _numbers[index];
+            set => _numbers[index] = value;
+        }
+
+        // Печать вектора
+        public void Print(print _del)
+        {
+            if (_del != null)
+            {
+                _del.Invoke(this);
+            }
+            else throw new Exception("Неверная ссылка на делегат");
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _numbers.GetEnumerator();
+        }
+
+        // Перегруженные операторы 
+        public static Vector<T> operator +(Vector<T> a, Vector<T> b)
+        {
+            if (a.Lengh == b.Lengh)
+            {
+                Vector<T> temp = new Vector<T>(a.Lengh);
+                for (int i = 0; i < a.Lengh; i++)
+                {
+                    temp[i] = (T)Convert.ChangeType((dynamic)a[i] + (dynamic)b[i], typeof(T));
+                }
+                return temp;
+            }
+            else
+            {
+                throw new Exception("Разный размер векторов");
+            }
+        }
+        public static Vector<T> operator -(Vector<T> a, Vector<T> b)
+        {
+            if (a.Lengh == b.Lengh)
+            {
+                Vector<T> temp = new Vector<T>(a.Lengh);
+                for (int i = 0; i < a.Lengh; i++)
+                {
+                    temp[i] = (T)Convert.ChangeType((dynamic)a[i] - (dynamic)b[i], typeof(T));
+                }
+                return temp;
+            }
+            else
+            {
+                throw new Exception("Разный размер векторов");
+            }
+        }
+        public static T operator *(Vector<T> a, Vector<T> b)
+        {
+            if (a.Lengh == b.Lengh)
+            {
+                T sum = default(T);
+
+                for (int i = 0; i < a.Lengh; i++)
+                {
+                    sum = (T)Convert.ChangeType((dynamic)sum + (dynamic)a[i] * (dynamic)b[i], typeof(T));
+                }
+
+                return sum;
+            }
+            else
+            {
+                throw new Exception("Разный размер векторов");
+            }
+        }
+        public static Vector<T> operator *(Vector<T> a, T b)
+        {
+            Vector<T> temp = new Vector<T>(a.Lengh);
+
+            for (int i = 0; i < a.Lengh; i++)
+            {
+                temp[i] = (T)Convert.ChangeType((dynamic)a[i] * (dynamic)b, typeof(T));
+            }
+
+            return temp;
+        }
+        public static Vector<T> operator *(T b, Vector<T> a)
+        {
+            Vector<T> temp = new Vector<T>(a.Lengh);
+
+            for (int i = 0; i < a.Lengh; i++)
+            {
+                temp[i] = (T)Convert.ChangeType((dynamic)a[i] * (dynamic)b, typeof(T));
+            }
+
+            return temp;
+        }
+    }
+}
